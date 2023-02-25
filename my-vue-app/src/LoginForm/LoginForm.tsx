c9edde4eac82b49834174ebe2e257bd8ad56119b
hays0503 <hays050pascal@gmail.com>
2023-02-26 00:19:22 +0600

    сделал отправку формы с данными

--- my-vue-app/src/LoginForm/LoginForm.tsx
+++ my-vue-app/src/LoginForm/LoginForm.tsx
@@ -1,27 +1,34 @@
+import React, { MouseEvent } from 'react';
+
 import './LoginFormStyle.css'
 import shied from './pic/shied.png'
 
+
+
 export function LoginForm(){
 
-    const handleMouseMove = (e:any) => {
-        const el = document.getElementById("wrapper");
-        const d = el.getBoundingClientRect();
-        let x = e.clientX - (d.left + Math.floor(d.width / 2));
-        let y = e.clientY - (d.top + Math.floor(d.height / 2));
-        // Invert values
-        x = x - x * 2;
-        y = y - y * 2;
-        document.documentElement.style.setProperty("--scale", "1.6");
-        document.documentElement.style.setProperty("--x", x / 2 + "px");
-    
-        document.documentElement.style.setProperty("--y", y / 2 + "px");
-      };
 
+
+    const handleMouseMove = (event: MouseEvent) => {
+        const el = document.getElementById("wrapper");
+        if(el){
+            const d = el.getBoundingClientRect();
+            let x = event.clientX - (d.left + Math.floor(d.width / 2));
+            let y = event.clientY - (d.top + Math.floor(d.height / 2));
+            // Invert values
+            x = x - x * 2;
+            y = y - y * 2;
+            document.documentElement.style.setProperty("--scale", "1.6");
+            document.documentElement.style.setProperty("--x", x / 2 + "px");
+        
+            document.documentElement.style.setProperty("--y", y / 2 + "px");
+        }
+    };
 
     const handleMouseLeave = () => {
-        document.documentElement.style.setProperty("--scale", 1);
-        document.documentElement.style.setProperty("--x", 0);
-        document.documentElement.style.setProperty("--y", 0);
+        document.documentElement.style.setProperty("--scale", "1");
+        document.documentElement.style.setProperty("--x", "0");
+        document.documentElement.style.setProperty("--y", "0");
       };
 
     return (<div className={"backgroundLoginForm"}>
@@ -31,7 +38,7 @@ export function LoginForm(){
 
                 <div    
                     id="wrapper" 
-                    onMouseMove={handleMouseMove}
+                    onMouseOver={handleMouseMove}
                     onClick={handleMouseLeave}
                 >
 
@@ -42,16 +49,16 @@ export function LoginForm(){
                             <h1>Форма логина</h1>
                             <h2>У вас есть аккаунт ?</h2>
 
-                            <form className={"centerBlock formDataDivider10"} autoComplete="on" action="" method="post">
-                                <input className={"inputArea"} placeholder="Логин"/>                
-                                <input className={"inputArea"} placeholder="Пароль"/>
-                                <button className={"inputArea"} name="button">Войти</button>      
-                            </form>
+                            <form className={"centerBlock formDataDivider10"} autoComplete="on" name="LoginForm" method="post">
+                                <input className={"inputArea"} name="login" placeholder="Логин"/>                
+                                <input className={"inputArea"} name="password" placeholder="Пароль"/>
+                                <button className={"inputArea"}>Войти</button>      
+                            
                             <br/>
                             <div className={"inLineBlock formDataDivider20"}>
 
                                 <label>Запомнить меня
-                                    <input type='checkbox'></input>
+                                    <input type='checkbox' name='remember-user'></input>
                                 </label>
 
                                 <div className="w-50 text-md-right">
@@ -61,6 +68,7 @@ export function LoginForm(){
                                 </div>
 
                             </div>
+                            </form>
                         </div>
                     </div>
                 </div>
